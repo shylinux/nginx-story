539f7ecfec7eae2eb6b2c9635acbe32701c582fd
shaoying <shaoying@zuoyebang.com>
2020-08-14 21:31:14 +0800

    opt some

---
+++ etc/miss.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# git &>/dev/null || yum install -y git
+
+[ -f ~/.ish/plug.sh ] || [ -f ./.ish/plug.sh ] || git clone https://github.com/shylinux/intshell ./.ish
+[ "$ISH_CONF_PRE" != "" ] || source ./.ish/plug.sh || source ~/.ish/plug.sh
+
+require show.sh
+require help.sh
+require miss.sh
+
+ish_miss_prepare_compile
+ish_miss_prepare_install
+# ish_miss_prepare_develop
+
+ish_miss_prepare_volcanos
+# ish_miss_prepare learning
+# ish_miss_prepare_icebergs
+# ish_miss_prepare toolkits
