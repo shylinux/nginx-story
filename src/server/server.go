de2583aefc276c1f6abd2f6558015bf6240f34d7
shaoying <shylinux@icloud.com>
2020-10-18 14:26:31 +0800

    opt code

--- src/server/server.go
+++ src/server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	ice "github.com/shylinux/icebergs"
 	"github.com/shylinux/icebergs/base/cli"
+	"github.com/shylinux/icebergs/base/gdb"
+	"github.com/shylinux/icebergs/base/web"
 	"github.com/shylinux/icebergs/core/code"
 	kit "github.com/shylinux/toolkits"
 
@@ -12,9 +14,9 @@ import (
 )
 
 const (
-	NGINX  = "nginx"
 	SERVER = "server"
 )
+const NGINX = "nginx"
 
 var Index = &ice.Context{Name: NGINX, Help: "nginx",
 	Configs: map[string]*ice.Config{
@@ -25,14 +27,14 @@ var Index = &ice.Context{Name: NGINX, Help: "nginx",
 		)},
 	},
 	Commands: map[string]*ice.Command{
-		SERVER: {Name: "server port=auto path=auto auto 启动:button 构建:button 下载:button", Help: "服务器", Action: map[string]*ice.Action{
-			"download": {Name: "download", Help: "下载", Hand: func(m *ice.Message, arg ...string) {
-				m.Cmdy(code.INSTALL, "download", m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)))
+		SERVER: {Name: "server port path auto start build download", Help: "服务器", Action: map[string]*ice.Action{
+			web.DOWNLOAD: {Name: "download", Help: "下载", Hand: func(m *ice.Message, arg ...string) {
+				m.Cmdy(code.INSTALL, web.DOWNLOAD, m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)))
 			}},
-			"build": {Name: "build", Help: "构建", Hand: func(m *ice.Message, arg ...string) {
-				m.Cmdy(code.INSTALL, "build", m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)))
+			gdb.BUILD: {Name: "build", Help: "构建", Hand: func(m *ice.Message, arg ...string) {
+				m.Cmdy(code.INSTALL, gdb.BUILD, m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)))
 			}},
-			"start": {Name: "start", Help: "启动", Hand: func(m *ice.Message, arg ...string) {
+			gdb.START: {Name: "start", Help: "启动", Hand: func(m *ice.Message, arg ...string) {
 				m.Optionv("prepare", func(p string) []string {
 					kit.Rewrite(path.Join(p, "conf/nginx.conf"), func(line string) string {
 						if strings.HasPrefix(strings.TrimSpace(line), "listen") {
@@ -42,16 +44,20 @@ var Index = &ice.Context{Name: NGINX, Help: "nginx",
 					})
 					return []string{"-p", kit.Path(p)}
 				})
-				m.Cmdy(code.INSTALL, "start", m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)), "sbin/nginx")
+				m.Cmdy(code.INSTALL, gdb.START, m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)), "sbin/nginx")
 			}},
-			"reload": {Name: "reload", Help: "重载", Hand: func(m *ice.Message, arg ...string) {
+			gdb.RELOAD: {Name: "reload", Help: "重载", Hand: func(m *ice.Message, arg ...string) {
 				p := m.Option(cli.CMD_DIR, kit.Path(path.Join(m.Conf(cli.DAEMON, kit.META_PATH), m.Option(kit.SSH_PORT))))
 				m.Cmdy(cli.SYSTEM, "sbin/nginx", "-p", p, "-s", "reload")
 			}},
 		}, Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {
-			m.Cmdy(code.INSTALL, path.Base(m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS))), arg)
+			m.Cmdy(code.INSTALL, m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)), arg)
 			if len(arg) == 0 || arg[0] == "" {
-				m.PushAction("重载")
+				m.Table(func(index int, value map[string]string, head []string) {
+					u := kit.ParseURL(m.Option(ice.MSG_USERWEB))
+					m.PushAnchor(kit.Format("http://%s:%s", u.Hostname(), value["port"]))
+					m.PushButton(gdb.RELOAD)
+				})
 			}
 		}},
 	},
